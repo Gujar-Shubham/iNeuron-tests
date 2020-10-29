6e4d999459418dba7f260bdc2a5b2de98ef7406f
Shubham arun gujar <42720425+Gujar-Shubham@users.noreply.github.com>
2020-10-29 11:37:54 +0530

    Add files via upload
    Assignment 3

---
+++ 1.2_MyFilter_3.py
@@ -0,0 +1,34 @@
+# Filter the even and odd number from list which are multiples  of 5
+
+#Input 
+
+print("Input:")
+number=int(input("Please insert the number: "))
+
+num_list=list(range(1,number+1))
+
+
+def myfilter(num_list):
+    '''This function will filter even and odd numbers from list which are multiples of 5 '''
+    num_even_list=[]
+    num_odd_list=[]
+    
+    for i in num_list:
+        if(i%5==0):
+            if(i%2==0):
+                num_even_list.append(i)
+            else:
+                num_odd_list.append(i)
+                
+    return num_even_list,num_odd_list
+
+
+#Function Execution
+output_value=myfilter(num_list)
+
+#Output
+
+print("Output:")
+print("List of numbers:",num_list)
+print("List of Even numbers, which are multiples of 5 are:",output_value[0])
+print("List of Odd numbers, which are multiples of 5 are:",output_value[1])
